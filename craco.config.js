f0705b677463a9734c4214c2f4832e29a8fad6c5
emad <emadi68moh@gmail.com>
2024-05-24 01:44:54 +0330

    fix aliases

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  webpack: {
-    alias: {
-      // '@': path.resolve(__dirname, 'src'),
-      '@/material': path.resolve(__dirname, 'src/lib/components/material'),
-      '@/elements': path.resolve(__dirname, 'src/lib/components/common/elements'),
-      '@/layouts': path.resolve(__dirname, 'src/lib/components/common/layouts'),
-      '@/providers': path.resolve(__dirname, 'src/lib/components/utility/providers'),
-      '@/features': path.resolve(__dirname, 'src/lib/components/features'),
-      '@/themes': path.resolve(__dirname, 'src/lib/themes'),
-      '@/types': path.resolve(__dirname, 'src/lib/types'),
-      '@/utils': path.resolve(__dirname, 'src/lib/utils'),
-      '@/assets': path.resolve(__dirname, 'src/lib/assets'),
-      '@/mock': path.resolve(__dirname, 'src/mock'),
+    webpack: {
+        alias: {
+            '@material': path.resolve(__dirname, 'src/lib/components/material'),
+            '@elements': path.resolve(__dirname, 'src/lib/components/common/elements'),
+            '@layouts': path.resolve(__dirname, 'src/lib/components/common/layouts'),
+            '@providers': path.resolve(__dirname, 'src/lib/components/utility/providers'),
+            '@features': path.resolve(__dirname, 'src/lib/components/features'),
+            '@themes': path.resolve(__dirname, 'src/lib/themes'),
+            '@types': path.resolve(__dirname, 'src/lib/types'),
+            '@utils': path.resolve(__dirname, 'src/lib/utils'),
+            '@assets': path.resolve(__dirname, 'src/lib/assets'),
+        },
     },
-  },
 };
