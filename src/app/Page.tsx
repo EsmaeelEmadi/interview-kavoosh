c9ddeb1e639a9aa28fa75d0c4ab5c65c4cad891c
emad <emadi68moh@gmail.com>
2024-05-26 15:31:25 +0330

    removed temporary components and added TodoBoard

--- src/app/Page.tsx
+++ src/app/Page.tsx
@@ -7,21 +7,18 @@ import { theme } from '@themes/material/mainTheme';
 // COMPONENTS
 import TodoBoard from '@features/todoBoard/TodoBoard';
 
-import { Typography } from '@material/index';
-
 // TYPES
 import type { FC } from 'react';
 
 const Page: FC = () => {
-    return (
-        <div className='App'>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Typography variant='h3'>Kavoosh</Typography>
-                <TodoBoard name='hello' />
-            </ThemeProvider>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <TodoBoard />
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default Page;
